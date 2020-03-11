532a6ef42880c8fcff8ca3b7515b4ca660a82837
zhangshiyuan <313997791@qq.com>
2020-03-11 16:32:09 +0800

    新增User

---
+++ src/main/java/com/dtjs/gitforbranch/entity/User.java
@@ -0,0 +1,13 @@
+package com.dtjs.gitforbranch.entity;
+
+import lombok.Data;
+
+/**
+ * @author zhangshiyuan
+ * @Date 2020/3/11 16:27
+ */
+@Data
+public class User {
+    private String name;
+    private String school;
+}
