33bce88ac3e0abc7b7c66dfd036478864efb1adf
zhangshiyuan <313997791@qq.com>
2020-03-11 15:58:38 +0800

    first commit

---
+++ src/main/java/com/dtjs/gitforbranch/GitforbranchApplication.java
@@ -0,0 +1,13 @@
+package com.dtjs.gitforbranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitforbranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitforbranchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dtjs/gitforbranch/GitforbranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtjs.gitforbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitforbranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
