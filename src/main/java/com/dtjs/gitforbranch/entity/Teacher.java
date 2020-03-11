43e375f30270c23187743d571d846a39ce8c382d
zhangshiyuan <313997791@qq.com>
2020-03-11 16:39:12 +0800

    新增Teacher

---
+++ src/main/java/com/dtjs/gitforbranch/entity/Teacher.java
@@ -0,0 +1,13 @@
+package com.dtjs.gitforbranch.entity;
+
+import lombok.Data;
+
+/**
+ * @author zhangshiyuan
+ * @Date 2020/3/11 16:37
+ */
+@Data
+public class Teacher {
+    private Integer number;
+    private String className;
+}
